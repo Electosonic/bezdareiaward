ac7499335834262b59ed5e035fb4df88f6f3b8c3
nikitnos2003 <nikitnos2003@gmail.com>
2025-12-25 06:33:20 +0300

    fix images path

--- src/ballot.ts
+++ src/ballot.ts
@@ -6,10 +6,10 @@ export const ballot: Nomination[] = [
     id: "zavoz_goda",
     title: "Завоз года — кандидаты",
     candidates: [
-      { id: "iris", title: "Браки в Ирисе", image: "/cat.png" },
-      { id: "sab", title: "Веном в САБчате", image: "/cat.png" },
-      { id: "blur", title: "Блюр Лизы в фильме", image: "/cat.png" },
-      { id: "tulpa", title: '"Тульпа" Вики', image: "/cat.png" },
+      { id: "iris", title: "Браки в Ирисе" },
+      { id: "sab", title: "Веном в САБчате",},
+      { id: "blur", title: "Блюр Лизы в фильме", },
+      { id: "tulpa", title: '"Тульпа" Вики', },
     ],
   },
 ];
