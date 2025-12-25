1ddc61454c9c6a2316aef55c92790e10d56861a3
nikitnos2003 <nikitnos2003@gmail.com>
2025-12-25 05:09:45 +0300

    fix site url

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const SITE_URL = "https://asukaska.github.io/bezdareiaward/";
+export const SITE_URL = "https://electosonic.github.io/bezdareiaward/";
 export const WORKER_URL = "https://bezdareiaward-worker.nikitnos2003.workers.dev";
